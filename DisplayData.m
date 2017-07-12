e948264de2890e390cd2344e25d2f89fb2f93397
SebFar <sebfar@gmail.com>
2017-07-12 12:50:57 +0100

    Removing old Octave version

--- DisplayData.m
+++
@@ -1,8 +0,0 @@
-#Display the outputs
-
-load("SARSAscores");
-scorerange = 1:250;
-plot(scorerange,scores(51:300),scorerange,eightscores(51:300),scorerange,ninescores(51:300),scorerange,stochscores(51:300));
-xlabel("Episode");
-ylabel("Steps used");
-title("Learning of SARSA agents in Windy Canyon, without first 50 episodes");
